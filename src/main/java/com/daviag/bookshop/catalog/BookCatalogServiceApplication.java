e3546a3508f7ce67871fefc5ec8984f8173b4c95
David V. <villarejod@gmail.com>
2024-09-21 11:09:58 +0200

    First commit

---
+++ src/main/java/com/daviag/bookshop/catalog/BookCatalogServiceApplication.java
@@ -0,0 +1,11 @@
+package com.daviag.bookshop.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookCatalogServiceApplication.class, args);
+    }
+}
