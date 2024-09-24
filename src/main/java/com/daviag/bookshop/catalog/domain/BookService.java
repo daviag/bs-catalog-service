cbb202dec0079cbad28db5ed7a005e5d9aa4ce0c
David V. <villarejod@gmail.com>
2024-09-24 11:52:03 +0200

    Basic jdbc config

--- src/main/java/com/daviag/bookshop/catalog/domain/BookService.java
+++ src/main/java/com/daviag/bookshop/catalog/domain/BookService.java
@@ -32,10 +32,16 @@ public class BookService {
         return bookRepository.findByIsbn(isbn)
                 .map(existingBook -> {
                     var bookToUpdate = new Book(
+                            existingBook.id(),
                             existingBook.isbn(),
                             book.title(),
                             book.author(),
-                            book.price());
+                            book.price(),
+                            existingBook.createdBy(),
+                            existingBook.createdDate(),
+                            existingBook.lastModifiedBy(),
+                            existingBook.lastModifiedDate(),
+                            existingBook.version());
                     return bookRepository.save(bookToUpdate);
                 })
                 .orElseGet(() -> addBookToCatalog(book));
--- src/test/java/com/daviag/bookshop/catalog/web/BookJsonTests.java
+++ src/test/java/com/daviag/bookshop/catalog/web/BookJsonTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
+import java.time.Instant;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
@@ -16,8 +18,11 @@ public class BookJsonTests {
 
     @Test
     void testSerialize() throws Exception {
-        var book = new Book("1234567890", "Title", "Author", 9.90);
+        var book = new Book(1L, "1234567890", "Title", "Author", 9.90,
+                "david", Instant.now(), "laura", Instant.now(), 1);
         var jsonContent = json.write(book);
+        assertThat(jsonContent).extractingJsonPathStringValue("@.id")
+                .isEqualTo(book.id());
         assertThat(jsonContent).extractingJsonPathStringValue("@.isbn")
                 .isEqualTo(book.isbn());
         assertThat(jsonContent).extractingJsonPathStringValue("@.title")
@@ -26,20 +31,42 @@ public class BookJsonTests {
                 .isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
                 .isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.price")
+                .isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.createdBy")
+                .isEqualTo(book.createdBy());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.createdDate")
+                .isEqualTo(book.createdDate());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.lastModifiedBy")
+                .isEqualTo(book.lastModifiedBy());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.lastModifiedDate")
+                .isEqualTo(book.lastModifiedDate());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.version")
+                .isEqualTo(book.version());
     }
 
     @Test
     void testDeserialize() throws Exception {
+        Instant instant = Instant.now();
+        System.out.println(instant);
         var content = """
                 {
+                  "id": 1,
                   "isbn": "1234567890",
                   "title": "Title",
                   "author": "Author",
-                  "price": 9.90
+                  "price": 9.90,
+                  "createdBy": "david",
+                  "createdDate": "%s",
+                  "lastModifiedBy": "laura",
+                  "lastModifiedDate": "%s",
+                  "version": 1
                 }
-                """;
+                """.formatted(instant, instant);
+        System.out.println(content);
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(new Book("1234567890", "Title", "Author", 9.90));
+                .isEqualTo(new Book(1L, "1234567890", "Title", "Author", 9.90,
+                        "david", instant, "laura", instant, 1));
     }
 }
